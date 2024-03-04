6d9652be1123e83a8f102c8b1599d3b62586a966
villsun <lonukkola@gmail.com>
2024-03-04 20:48:23 +0100

    Add task 1

--- Lesson_12(homework)/Homework.cpp
+++ Lesson_12(homework)/Homework.cpp
@@ -1,12 +1,44 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string.h>
+
 
 using namespace std;
 
 int main()
 {
+	// Завдання 1
 
+	FILE* p_file_1 = fopen(R"(F:\text.txt)", "r");
+	FILE* p_file_2 = fopen(R"(E:\text.txt)", "w");
+	char c;
+	int counter = 0;
+	char str_temp[50] = "";
+
+	if (p_file_1 == nullptr)
+	{
+		return -1;
+	}
+
+	do
+	{
+		c = fgetc(p_file_1);
+		if (c != '\n')
+		{
+			str_temp[counter++] = c;
+		}
+		else
+		{
+			str_temp[counter] = '\0';
+			if (counter > 7)
+			{
+				fprintf(p_file_2, "%s\n", str_temp);
+			}
+			counter = 0;
+		}
+	} while (c != EOF);
+
+	fclose(p_file_1);
+	fclose(p_file_2);
 
 	return 0;
 }
